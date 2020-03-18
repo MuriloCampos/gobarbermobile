af52c4b7b62e6fcf852c0825e67336940e331be9
Murilo Campos <murilo13campos@gmail.com>
2020-03-18 10:17:48 -0400

    styling sign in page

--- src/routes.js
+++ src/routes.js
@@ -37,7 +37,11 @@ export default function Routes() {
             <Tab.Screen name="Settings" component={SettingsScreen} />
           </>
         ) : (
-          <Stack.Screen name="SignIn" component={SignIn} />
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{ headerShown: false }}
+          />
         )}
       </Stack.Navigator>
     </NavigationContainer>
