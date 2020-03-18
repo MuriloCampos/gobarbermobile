db296dd27dc1bb90e0a4260b2842e4f6cdac1c24
Murilo Campos <murilo13campos@gmail.com>
2020-03-18 10:54:42 -0400

    configuring reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import React from 'react';
+import './config/ReactotronConfig';
 
 import Routes from './routes';
 
