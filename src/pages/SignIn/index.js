9a2d5d9e9c5067f0f85210036e054af4b001f452
Murilo Campos <murilo13campos@gmail.com>
2020-03-18 09:54:51 -0400

    creating button and text input components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>SignIn Page</Text>;
+  return (
+    <Background>
+      <Text>SignIn Page</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
+    </Background>
+  );
 }
