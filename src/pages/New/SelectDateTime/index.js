2aeb64e4506e587dc1c1e6e509e52a554fa37c53
Murilo Campos <murilo13campos@gmail.com>
2020-03-19 04:36:23 -0400

    styling routes to create a new appointment

---
+++ src/pages/New/SelectDateTime/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { View } from 'react-native';
+
+import Background from '~/components/Background';
+import DateInput from '~/components/DateInput';
+
+import { Container } from './styles';
+
+export default function SelectDateTime() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <Background>
+      <Container>
+        <DateInput date={date} onChange={setDate} />
+      </Container>
+    </Background>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -11,8 +12,56 @@ import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
 
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
+
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function newSchedule({ navigation }) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        headerTintColor: '#FFF',
+        headerLeftContainerStyle: { marginLeft: 20 },
+      }}
+    >
+      <Stack.Screen
+        name="Selecione o Provedor"
+        component={SelectProvider}
+        options={{
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('Agendamentos');
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Selecione o horario"
+        component={SelectDateTime}
+        options={{
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Icon name="chevron-left" size={20} color="#FFF" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen name="Confirm" component={Confirm} />
+    </Stack.Navigator>
+  );
+}
 
 export default function Routes({ isLoggedIn }) {
   return (
@@ -34,6 +83,21 @@ export default function Routes({ isLoggedIn }) {
               tabBarLabel: 'Agendamentos',
               tabBarIcon: ({ color, size }) => (
                 <Icon name="event" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Agendar"
+            component={newSchedule}
+            options={{
+              tabBarLabel: 'Agendar',
+              tabBarVisible: false,
+              tabBarIcon: ({ color, size }) => (
+                <Icon
+                  name="add-circle-outline"
+                  color="rgba(255,255,255,0.6)"
+                  size={size}
+                />
               ),
             }}
           />
